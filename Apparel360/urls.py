10b7a51cbbbdd57c56f046fb2dab58122a6f34aa
Diksha <dgaonkar@unomaha.edu>
2020-12-15 12:18:27 -0500

    Removed Dashboard Updated Footer

--- Apparel360/urls.py
+++ Apparel360/urls.py
@@ -42,7 +42,7 @@ urlpatterns = [
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
     path('user_profile/', UserViews.user_update, name = 'user_profile'),
     path('update_user_information', UserViews.user_update, name = 'update_user_information'),
-    path('dashboard', UserViews.dashboard, name='dashboard'),
+
 
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
